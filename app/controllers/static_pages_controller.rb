dc39c9f45ffd37b504bd6c75b40244abc538e8f3
cw79 <corie.wiren@gmail.com>
2015-12-13 15:50:43 +0100

    Created controller page.

--- app/controllers/static_pages_controller.rb
+++ app/controllers/static_pages_controller.rb
@@ -1,4 +1,20 @@
 class StaticPagesController < ApplicationController
+	def initialize
+  		@bars = {
+  			1 => Bar.new(1, "100%", ["unsweetened", "intense", "dark"]),
+		  	2 => Bar.new(2, "91%", ["bitter", "dark", "robust", "smoky", "full"])
+  		}
+  		super
+  	end
+
   def index
+	  @characteristics = Array.new()
+	  @bars.each do |id, bar|
+	  	puts "#{id} #{bar.name}"
+	  	bar.characteristics.each do |character|
+	  		puts character
+	  	  	@characteristics.push(character)
+	  	end
+	  end
   end
 end
---
+++ app/models/bar.rb
@@ -0,0 +1,19 @@
+class Bar
+	def initialize(id, name, characteristics)
+		@id = id
+		@name = name
+		@characteristics = characteristics
+	end
+
+	def id
+		return @id
+	end
+
+	def name
+		return @name
+	end
+
+	def characteristics
+		return @characteristics
+	end
+end
