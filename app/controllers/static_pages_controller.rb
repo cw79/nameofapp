6901f5d81d538694ef3d044b2223b0ff0a1a112b
cw79 <corie.wiren@gmail.com>
2016-01-24 12:53:07 +0100

    Added styling for contact form. Set up automatic email response.

--- app/controllers/static_pages_controller.rb
+++ app/controllers/static_pages_controller.rb
@@ -47,10 +47,7 @@ class StaticPagesController < ApplicationController
 		@name = params[:name]
 		@email = params[:email]
 		@message = params[:message]
-		ActionMailer::Base.mail(:from => @email,
-			:to => 'corie.wiren@gmail.com',
-			:subject => "A new contact form message from #{@name}",
-			:body => @message).deliver_now
+		UserMailer.contact_form(@email, @name, @message).deliver_now
 	end
 end
 
