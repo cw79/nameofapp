2b41c8111551f80543ca5d38b16bcc81ff732fa8
cw79 <corie.wiren@gmail.com>
2016-01-25 21:30:17 +0100

    added automated email upon user sign up.

--- app/mailers/user_mailer.rb
+++ app/mailers/user_mailer.rb
@@ -1,10 +1,16 @@
 class UserMailer < ApplicationMailer
-  default from: "corie.wiren@gmail.com"
+	default from: "corie.wiren@gmail.com"
 
-  def contact_form(email, name, message)
-  @message = message
-    mail(:from => email,
-        :to => "corie.wiren@gmail.com",
-        :subject => "A new contact form message from #{name}")
-  end
+	def contact_form(email, name, message)
+		@message = message
+		mail(:from => email,
+			:to => "corie.wiren@gmail.com",
+			:subject => "A new contact form message from #{name}")
+	end
+
+	def welcome(user)
+		@appname = "NiCho Chocolate"
+		mail( :to => user.email,
+			:subject => "Welcome to #{@appname}!")
+	end
 end
